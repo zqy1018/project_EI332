6441acece7f324ef03ccce7d72c97177416c3e2c
zqy1018 <979115525@qq.com>
2020-06-07 15:59:35 +0800

    updated

--- single_cycle_cpu_with_IO/sc_computer.v
+++ single_cycle_cpu_with_IO/sc_computer.v
@@ -1,12 +1,27 @@
-module sc_computer (resetn, clk, 
-    sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9,
-    HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
-    // 第一行对应一些接线，第二行对应 I/O 接口
-   
-    input               resetn, clk;
-    input               sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9;
+`default_nettype none
 
-    output  [6: 0]      HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+module sc_computer (
+    input resetn, 
+    input clk,
+    input sw0, 
+    input sw1, 
+    input sw2, 
+    input sw3, 
+    input sw4, 
+    input sw5, 
+    input sw6, 
+    input sw7, 
+    input sw8, 
+    input sw9, 
+    
+    output wire [6: 0] HEX0, 
+    output wire [6: 0] HEX1, 
+    output wire [6: 0] HEX2, 
+    output wire [6: 0] HEX3, 
+    output wire [6: 0] HEX4, 
+    output wire [6: 0] HEX5
+);
+    // 第一行对应一些接线，第二行对应 I/O 接口
 
     wire    [31: 0]     pc, inst, aluout, memout;
     wire                imem_clk, dmem_clk;
@@ -19,14 +34,29 @@ module sc_computer (resetn, clk,
     in_port             inst1(sw0, sw1, sw2, sw3, sw4, in_port1);
     in_port             inst2(sw5, sw6, sw7, sw8, sw9, in_port0);
 
-    clock_and_mem_clock inst3(clk, clock_out, mem_clk);
-    // clock_and_mem_clock(main_clk, clock_out, mem_clk);
+    clock_and_mem_clock inst3(
+        .main_clk(clk), 
+        .clock_out(clock_out), 
+        .mem_clk(mem_clk)
+    );
 
-    sc_computer_main    inst4(resetn, clock_out, mem_clk, pc, inst, aluout, memout, imem_clk, dmem_clk, 
-                                in_port0, in_port1, out_port0, out_port1, out_port2);
-    // sc_computer_main (resetn, clock, mem_clk, pc, inst, aluout, memout, imem_clk, dmem_clk, 
-    // in_port0, in_port1, out_port0, out_port1, out_port2, 
-    // mem_dataout, io_read_data);
+    sc_computer_main    inst4(
+        .resetn(resetn), 
+        .clock(clock_out), 
+        .mem_clk(mem_clk), 
+        .in_port0(in_port0), 
+        .in_port1(in_port1), 
+
+        .pc(pc), 
+        .inst(inst), 
+        .aluout(aluout), 
+        .memout(memout), 
+        .imem_clk(imem_clk), 
+        .dmem_clk(dmem_clk), 
+        .out_port0(out_port0), 
+        .out_port1(out_port1), 
+        .out_port2(out_port2)
+    );
 
     out_port_seg        inst5(out_port0, HEX1, HEX0);
     out_port_seg        inst6(out_port1, HEX3, HEX2);
--- single_cycle_cpu_with_IO/sc_computer_main.v
+++ single_cycle_cpu_with_IO/sc_computer_main.v
@@ -1,29 +1,51 @@
-module sc_computer_main (resetn, clock, mem_clk, pc, inst, aluout, memout, imem_clk, dmem_clk, 
-    in_port0, in_port1, out_port0, out_port1, out_port2);
-   
-    input               resetn, clock, mem_clk; 
-    input   [31: 0]     in_port0, in_port1;
+`default_nettype none
 
-    // 现在由 clock_and_mem_clock 产生
-    output  [31: 0]     pc, inst, aluout, memout;
-    output              imem_clk, dmem_clk;
-    output  [31: 0]     out_port0, out_port1, out_port2;
-    // TODO：这里要不要把 mem_out 什么的加上去？
-    
-    wire    [31: 0]     pc, inst, aluout, memout;
-    wire                imem_clk, dmem_clk;
-    wire    [31: 0]     out_port0, out_port1, out_port2;
+module sc_computer_main (
+    input resetn, 
+    input clock, 
+    input mem_clk, 
+    input [31: 0] in_port0, 
+    input [31: 0] in_port1, 
+
+    output wire [31: 0] pc, 
+    output wire [31: 0] inst, 
+    output wire [31: 0] aluout,
+    output wire [31: 0] memout,
+    output wire imem_clk, 
+    output wire dmem_clk, 
+    output wire [31: 0] out_port0, 
+    output wire [31: 0] out_port1, 
+    output wire [31: 0] out_port2
+);
     wire    [31: 0]     data;
     wire                wmem; 
 
     sc_cpu              cpu(clock, resetn, inst, memout, pc, wmem, aluout, data); 
     // sc_cpu(clock, resetn, inst, mem, pc, wmem, alu, data);
-    sc_instmem          imem(pc, inst, clock, mem_clk, imem_clk); 
-    sc_datamem          dmem(resetn, aluout, data, memout, wmem, clock, mem_clk, dmem_clk, 
-                            out_port0, out_port1, out_port2, in_port0, in_port1); 
-    // sc_datamem (addr, datain, dataout, we, clock, mem_clk, dmem_clk, 
-    // out_port0, out_port1, out_port2, in_port0, in_port1, 
-    // mem_dataout, io_read_data);
+    sc_instmem imem(
+        .addr(pc), 
+        .clock(clock), 
+        .mem_clk(mem_clk), 
+        .inst(inst), 
+        .imem_clk(imem_clk)
+    ); 
+
+    sc_datamem dmem(
+        .resetn(resetn),
+        .we(wmem), 
+        .clock(clock), 
+        .mem_clk(mem_clk),  
+        .addr(aluout), 
+        .datain(data), 
+        .in_port0(in_port0), 
+        .in_port1(in_port1), 
+
+        .dmem_clk(dmem_clk), 
+        .dataout(memout), 
+        .out_port0(out_port0), 
+        .out_port1(out_port1), 
+        .out_port2(out_port2)
+    ); 
 
 endmodule
 
