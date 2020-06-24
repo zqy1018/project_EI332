56ac01bce01d4dcefa070587d7db56f153fefd4d
zqy1018 <979115525@qq.com>
2020-06-24 15:29:12 +0800

    fixed a critical typo

--- pipelined/sc_cu.v
+++ pipelined/sc_cu.v
@@ -94,7 +94,7 @@ module sc_cu (
    
     assign aluc[3] = i_sra;
     assign aluc[2] = i_sub | i_or | i_srl | i_sra | i_ori | i_lui;
-    assign aluc[1] = i_xor | i_sll | i_srl | i_sra | i_xori | i_lui;
+    assign aluc[1] = i_xor | i_sll | i_srl | i_sra | i_xori | i_lui | i_beq | i_bne;
     assign aluc[0] = i_and | i_or | i_sll | i_srl | i_sra | i_andi | i_ori;
     // ALU 的控制信号，每种组合表示一种运算
 
